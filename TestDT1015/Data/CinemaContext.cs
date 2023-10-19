90a7131fea717f769741bbeae32a3787ba027f68
bedalios48 <98773788+bedalios48@users.noreply.github.com>
2023-10-19 12:51:48 +0300

    link to movies added

--- TestDT1015/Data/CinemaContext.cs
+++ TestDT1015/Data/CinemaContext.cs
@@ -10,14 +10,5 @@ namespace TestDT1015.Data
         }
 
         public DbSet<Models.Movie> Movie { get; set; }
-
-        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-/*#warning To protect potentially sensitive information in your connection string, 
-            you should move it out of source code.See http://go.microsoft.com/fwlink/?LinkId=723263 
-     for guidance on storing connection strings.*/
-
-         /*   optionsBuilder.UseMySQL("Server=(localdb)\\mssqllocaldb;Database=CinemaContext-55912999-2c8f-46d3-99cc-bf932c1d13fe;Trusted_Connection=True;MultipleActiveResultSets=true");
-        }*/
     }
 }
